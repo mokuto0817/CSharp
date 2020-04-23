9178a00ed8440829163d9ece6d9d377c61a6b083
mokuto0817 <mokuto0817@icloud.com>
2020-04-23 22:06:27 +0800

    HW9

---
+++ HW9/CrawlerForm/CrawlerForm/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CrawlerForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Finish = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_url = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Finish
+            // 
+            this.Finish.FormattingEnabled = true;
+            this.Finish.ItemHeight = 18;
+            this.Finish.Location = new System.Drawing.Point(28, 115);
+            this.Finish.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Finish.Name = "Finish";
+            this.Finish.Size = new System.Drawing.Size(779, 364);
+            this.Finish.TabIndex = 7;
+            this.Finish.SelectedIndexChanged += new System.EventHandler(this.lbxFinish_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "网址：";
+            // 
+            // textBox_url
+            // 
+            this.textBox_url.Location = new System.Drawing.Point(88, 36);
+            this.textBox_url.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_url.Name = "textBox_url";
+            this.textBox_url.Size = new System.Drawing.Size(502, 28);
+            this.textBox_url.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 515);
+            this.Controls.Add(this.Finish);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_url);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Finish;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_url;
+    }
+}
+
---
+++ HW9/CrawlerForm/CrawlerForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using SimpleCrawler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CrawlerForm
+{
+    public partial class Form1 : Form
+    {
+
+        private Crawler crawler;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void AddUrl(string url)
+        {
+            Finish.Items.Add(url);
+        }
+        private void Crawler_PageDownloaded(string url)
+        {
+            if (this.Finish.InvokeRequired)
+            {
+                Action<String> action = this.AddUrl;
+                this.Invoke(action, new object[] { url });
+            }
+            else
+            {
+                AddUrl(url);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Finish.Items.Clear();
+            crawler = new Crawler(textBox_url.Text);
+            crawler.PageDownloaded += Crawler_PageDownloaded;
+            new Thread(crawler.Crawl).Start();
+        }
+
+        private void textBox_url_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbxFinish_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
